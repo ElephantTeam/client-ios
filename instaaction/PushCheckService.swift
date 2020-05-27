90ba31f3ca8a5e69c40282a08801a57f62aff51f
Michael Apanowicz <apan06@gmail.com>
2020-05-27 14:04:20 +0200

    Apanowicz/5 add fake push pulling (#8)
    * add Push Check Service
    
    * add fake push pull
    
    Co-authored-by: Michał Apanowicz <michal.apanowicz@schibsted.pl>

---
+++ instaaction/PushCheckService.swift
@@ -0,0 +1,57 @@
+//
+//  PushCheckService.swift
+//  instaaction
+//
+//  Created by Michał Apanowicz on 27/05/2020.
+//  Copyright © 2020 Schibsted. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+struct Challenges: Decodable, CustomStringConvertible {
+    var description: String {
+        challenges.compactMap { $0.description }.joined()
+    }
+
+    let challenges: [Challenge]
+}
+
+struct Challenge: Decodable, Hashable, CustomStringConvertible {
+    let name: String
+
+    var description: String {
+        name
+    }
+}
+
+final class PushCheckService {
+
+    static let shared = PushCheckService()
+
+    var subject = PassthroughSubject<Challenges, Never>()
+
+    private var timer: Timer?
+    private let webService = WebService()
+    private var cancelable: AnyCancellable?
+
+    init() {
+        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            let request = URLRequest(url: URL(string: "https://www.mocky.io/v2/5ece46f33000002a00ea0fe2")!)
+            let future: Future<Challenges, Error> = self.webService.load(request: request)
+
+            self.cancelable = future.sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error \(error)")
+                    break
+                }
+            }) { [weak self] value in
+                self?.subject.send(value)
+            }
+        }
+    }
+}
